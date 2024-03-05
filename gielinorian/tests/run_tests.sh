5117d06c0e969ddc270d8184e080909277afea82
Dean Verjans <developer.dean@outlook.com>
2024-03-05 14:47:04 +0100

    🔧 Update CI & env loading

---
+++ gielinorian/tests/run_tests.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+PACT="${PACT_PATH:-pact}"
+
+# Find all .repl files in the current directory
+REPL_SCRIPTS=$(find . ! -name "gielinorian.repl" -name "*.repl")
+
+for repl in $REPL_SCRIPTS
+  do echo "============================================================"
+     echo "Running $(grep -oP "[^/]*$" <<< $repl)"
+     echo "============================================================"
+     ${PACT} $repl 2>&1
+     echo ""
+done
